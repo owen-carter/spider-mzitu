b3fd90c80d4133b73c76fae1c2c1f7bdfe8cfe1e
owen-carter <owenjiao@outlook.com>
2017-09-23 22:12:23 +0800

    add gitignored

--- bin/download.js
+++ bin/download.js
@@ -1,15 +1,25 @@
+const fs = require('fs');
+const request = require('request');
 const logger = require('./log.conf');
 
 
 class wGeter {
     constructor() {
         this.taskList = [];
+        this.taskNumber = 0;
     }
 
     init() {
         process.on('message', (tasks) => {
             this.addTask(tasks)
-        })
+        });
+
+        setInterval(() => {
+            if (this.taskList.length > 1 && this.taskNumber < 20) {
+                this.curl(this.taskList.shift())
+                this.taskNumber++;
+            }
+        }, 1000 * 3)
 
     }
 
@@ -18,7 +28,24 @@ class wGeter {
     }
 
     curl(url) {
-        logger.info(`download ${url}`)
+        let stream, filename, path;
+        filename = url.split('/');
+        filename = filename.pop();
+        path = './images/' + filename
+        logger.info(`downloading ${filename}`);
+
+        stream = fs.createWriteStream(path);
+        request({
+            url: url,
+            headers: {'Referer': 'http://www.mzitu.com/103478'}
+        })
+            .pipe(stream)
+            .on('error', (err) => {
+                logger.error(err)
+            })
+            .on('close', () => {
+                this.taskNumber--;
+            });
     }
 
     run() {
