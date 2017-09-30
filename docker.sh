c9d4d5b628bea7c061857795144181bfdd05b107
owen-carter <owenjiao@outlook.com>
2017-09-30 18:26:25 +0800

    add docker

--- docker.sh
+++ docker.sh
@@ -1,2 +1,28 @@
 #!/usr/bin/env bash
-docker build -t app.dmg .
+docker run --name statusbarcontainer -d -p 80:8000 statusbarimg
+
+build(){
+    docker build -t app.dmg .
+}
+
+start(){
+    docker run --name statusbarcontainer -d -p 80:8000 statusbarimg
+}
+
+
+cmdList=( "build" "start" "exit" )
+
+OPTION=$(${window} --title "Docker Menu Dialog" --menu "Choose your option" 10 60 4 \
+"0" "just build" \
+"1" "just run" \
+"2" "exit"  3>&1 1>&2 2>&3)
+
+exitStatus=$?
+if [ ${exitStatus} = 0 ]; then
+    echo "Your chosen option: #${OPTION} - ${cmdList[OPTION]}"
+    ${cmdList[OPTION]}
+else
+    echo "You chose Cancel."
+fi
+exit 0
+
